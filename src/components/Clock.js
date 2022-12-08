c21b44eb30480e3cd2428f9542575672a15dac3c
Saka <email>
2022-12-08 19:45:11 +0700

    ClockFinishing

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Clock() {
-  return (
-    <div>Clock</div>
-  )
+  const [clockState, setClockState] = useState();
+
+  useEffect(() => {
+      setInterval(() => {
+        const date = new Date();
+        setClockState(date.toLocaleTimeString());
+    }, 1000);
+  }, []);
+  
+
+
+  return <div>{clockState}</div>
+
 }
 
 export default Clock
